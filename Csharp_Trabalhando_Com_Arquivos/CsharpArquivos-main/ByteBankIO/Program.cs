e9d33cf5e28500375e75948ae63092405072d386
Arielson Braga <ariel.braga09@gmail.com>
2025-11-12 16:27:19 -0300

    Finalizando o Curso CSharp_Trabalhando_Com_Arquivos

--- Csharp_Trabalhando_Com_Arquivos/CsharpArquivos-main/ByteBankIO/Program.cs
+++ Csharp_Trabalhando_Com_Arquivos/CsharpArquivos-main/ByteBankIO/Program.cs
@@ -5,25 +5,28 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        var enderecoDoArquivo = "contas.txt";
-        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-        {
-            var leitor =new StreamReader(fluxoDeArquivo);
-
-            //var linha = leitor.ReadLine(); le a linha
-
-            //var texto = leitor.ReadToEnd(); le o arquivo completo
-
-            //var numero = leitor.Read(); le o primeiro byte
-
-            while(!leitor.EndOfStream )
-            {
-                var linha = leitor.ReadLine();
-                Console.WriteLine(linha);
-            }
+        //UsarStreamDeEntrada();
 
 
-        }
+        //Console.WriteLine("Digite um nome: ");
+        //var nome = Console.ReadLine();
+
+
+        //var linhas = File.ReadAllLines("contas.txt");
+        //Console.WriteLine(linhas);
+
+        //foreach (var line in linhas)
+        //{
+        //    Console.WriteLine(line);
+        //}
+
+        var bytesArquivo = File.ReadAllBytes("contas.txt");
+        Console.WriteLine($"O Arquvio possui {bytesArquivo.Length} bytes");
+
+        File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+
+        Console.WriteLine("Aplicacao Finalizada");
+
         Console.ReadLine();
     }
 }
