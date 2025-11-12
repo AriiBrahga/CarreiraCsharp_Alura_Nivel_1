91a267bb4d86f3d7536b978c3e7564f838b049d7
Arielson Braga <ariel.braga09@gmail.com>
2025-11-12 09:25:58 -0300

    Iniciando o Curso Csharp Trabalhando com Arquivos

---
+++ Csharp_Trabalhando_Com_Arquivos/CsharpArquivos-main/ByteBankIO/LidandoComFileStreamDiretamente.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using ByteBankIO;
+
+partial class Program
+{
+    static void LidandoComFileStreamDiretamente()
+    {
+        var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            // 1 KB
+            var buffer = new byte[1024];
+
+            while (numeroDeBytesLidos != 0)
+            {
+
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                Console.WriteLine($"Bytes Lidos {numeroDeBytesLidos} ");
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+
+            }
+
+
+            //Devoluções
+            // O numero total de bytes lidos do buffer. Isso poderá ser menor que o numero de
+            // bytes solicitando se esse byneri de bystes não estiver disponivel no momento,
+            // ou zero, se o final do fluxo for atingido
+
+
+
+            fluxoDoArquivo.Close();
+
+            Console.ReadLine();
+        }
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
+        Console.Write(texto);
+        /*
+        foreach (byte b in buffer)
+        {
+            Console.Write(b);
+            Console.Write(" ");
+        }*/
+    }
+}
---
+++ Csharp_Trabalhando_Com_Arquivos/CsharpArquivos-main/ByteBankIO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using ByteBankIO;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor =new StreamReader(fluxoDeArquivo);
+
+            //var linha = leitor.ReadLine(); le a linha
+
+            //var texto = leitor.ReadToEnd(); le o arquivo completo
+
+            //var numero = leitor.Read(); le o primeiro byte
+
+            while(!leitor.EndOfStream )
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+
+
+        }
+        Console.ReadLine();
+    }
+}
