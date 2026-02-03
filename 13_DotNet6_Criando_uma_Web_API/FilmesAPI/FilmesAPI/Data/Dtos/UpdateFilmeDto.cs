fe2b2a6e0b24e173280e58c8738a961b61cbd34f
Arielson Braga <ariel.braga09@gmail.com>
2026-02-03 16:01:57 -0300

    Curso 13_13_DotNet6_Criando_uma_Web_API

---
+++ 13_DotNet6_Criando_uma_Web_API/FilmesAPI/FilmesAPI/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class UpdateFilmeDto
+    {
+
+        [Required(ErrorMessage = "O Título do filme é obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Gênero do filme é obrigatorio")]
+        [StringLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+        public string Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duracao deve ter entre 70 a 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
